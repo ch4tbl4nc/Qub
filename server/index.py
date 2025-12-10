a6ab32d27b0b6b0bdc2e434b640c39059e3ef994
leMecha <193751621+TimDcmtr@users.noreply.github.com>
2025-12-10 09:17:45 +0100

    Add security with .env variables

--- server/index.py
+++ server/index.py
@@ -1,6 +1,5 @@
 
 from fastapi import FastAPI
-from server.libs.database.Config import run_query
 from api.users import router as users_router
 
 # Create an instance of the FastAPI application
--- server/libs/database/Config.py
+++ server/libs/database/Config.py
@@ -1,11 +1,16 @@
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def _db_config():
     """Configuration de la base de données. !!! POUR DEV SINON UTILISER DES VARIABLES D'ENVIRONNEMENT !!!"""
     return {
-        'user': 'qubAdmin',
-        'password': '5c8e1c1e7b91ac191454',
-        'host': 'panel.lemecha.fr',
-        'port': 3309,
-        'database': 'qub',
+        'user': os.environ.get('DB_USER'),
+        'password': os.environ.get('DB_PASS'),
+        'host': os.environ.get('DB_HOST'),
+        'port': os.environ.get('DB_PORT'),
+        'database': os.environ.get('DB_NAME'),
         'raise_on_warnings': True
     }
