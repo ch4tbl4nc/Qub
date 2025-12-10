131eb6cf25380575b94c9c11bf8bb71c82970b7e
leMecha <193751621+TimDcmtr@users.noreply.github.com>
2025-12-10 08:32:31 +0100

    Base API

--- server/api/users.py
+++ server/api/users.py
@@ -9,3 +9,16 @@ router = APIRouter()
 @router.get("/")
 async def root():
     return {"message": "Hello Users !"}
+
+@router.get("/test")
+async def get_all_users():
+    query = "INSERT INTO users (id, username) VALUES (%s, %s)"
+    params = (1, 'testuser')
+
+    try:
+        run_query(query, params)
+        return {"message": "User inserted successfully."}
+    except Exception as e:
+        return {"error": str(e)}    
+
+
