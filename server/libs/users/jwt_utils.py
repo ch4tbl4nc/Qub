91eab6722faf84895aa00bd4ea2543c4f52da459
ch4tbl4nc <0xch4tbl4nc@proton.me>
2025-12-11 08:47:33 +0100

    Correction JWT 2

--- server/libs/users/jwt_utils.py
+++ server/libs/users/jwt_utils.py
@@ -10,7 +10,7 @@ JWT_ACCESS_TOKEN_TIMES = os.getenv("JWT_ACCESS_TOKEN_TIMES")  # minutes
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=JWT_ACCESS_TOKEN_TIMES))
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=int(JWT_ACCESS_TOKEN_TIMES)))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
 
