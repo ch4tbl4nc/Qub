6a39139b4685838591526ba7b810045156ab3eff
ch4tbl4nc <0xch4tbl4nc@proton.me>
2026-01-05 13:37:38 +0100

    Debug

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
 import webview
-
 window = webview.create_window('QUB', '../QUB/client/views/login.html', width=1200, height=800)
-
-<<<<<<< HEAD
-webview.start(http_server=True)
-=======
-webview.start(window, http_server=True, debug=True, )
->>>>>>> 4b4505a2a08d72a90e9539e1dd4f058c1ec0074c
+webview.start(http_server=True, debug=False)
